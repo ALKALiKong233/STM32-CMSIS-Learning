2e652fb9ff30633c4d36789a2438abf37d72faa4
ALKALiKong <alikongalk@gmail.com>
2025-08-05 15:02:50 +0800

    libs: delay: Make sure delay is inited before using it.

--- STM32-CMSIS-Libs/libs/delay/delay.c
+++ STM32-CMSIS-Libs/libs/delay/delay.c
@@ -1,15 +1,19 @@
 #include "delay.h"
+#include <stdbool.h>
 #include <stdint.h>
 
 volatile uint32_t systick_counter = 0;
+volatile bool is_delay_inited = false;
 
 void delay_init() {
+    if ( is_delay_inited ) return;
     SysTick->CTRL |= SysTick_CTRL_ENABLE |
                     SysTick_CTRL_TICKINT |
                     SysTick_CTRL_CLKSOURCE;
     /* Our MCU is at 72MHz, so set load to this for 1ms timer */
     SysTick->LOAD = 72000 - 1;
     SysTick->VAL = 0;
+    is_delay_inited = true;
 }
 
 void SysTick_Handler() {
@@ -17,10 +21,12 @@ void SysTick_Handler() {
 }
 
 uint32_t delay_get_tick() {
+    if ( !is_delay_inited ) delay_init();
     return systick_counter;
 }
 
 void delay_ms(uint32_t ms) {
+    if ( !is_delay_inited ) delay_init();
     uint32_t start = systick_counter;
     while (systick_counter - start < ms);
     return;
@@ -28,6 +34,7 @@ void delay_ms(uint32_t ms) {
 
 // t: expiration time, prd: period, now: current time. Return true if expired
 bool timer_expired(uint32_t *t, uint32_t prd, uint32_t now) {
+    if ( !is_delay_inited ) delay_init();
     if (now + prd < *t) *t = 0;                    // Time wrapped? Reset timer
     if (*t == 0) *t = now + prd;                   // First poll? Set expiration
     if (*t > now) return false;                    // Not expired yet, return
